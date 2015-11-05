56099125c2b6ff6e8743d1e805be9e3257e27fb0
Juan Correa <wuazopax@gmail.com>
2015-11-04 22:16:36 -0300

    Primer Comit Guido

---
+++ Assets/Scripts/MoverConViento.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverConViento : MonoBehaviour {
+
+	float randomSpeed;
+	[HideInInspector] public Transform t;
+
+
+	void Awake()
+	{
+		randomSpeed = Random.Range(0.05f , 0.035f);
+		foreach(Transform ta in transform)
+		{
+			t = ta;
+		}
+		foreach(Transform ta in t)
+		{
+			Vector2 screenPos = Camera.main.WorldToScreenPoint(ta.position) * -1;
+			ta.gameObject.GetComponent<SpriteRenderer>().sortingOrder = (int)screenPos.y; 
+		}
+
+
+	}
+
+	void FixedUpdate()
+	{
+		float fuerza = MicrophoneInput.instance.ruido * randomSpeed;
+		if(fuerza > 0.01) t.localPosition = new Vector3(t.localPosition.x , t.localPosition.y + fuerza , 0);
+	}
+
+	void OnDisable()
+	{
+		// resetear posicion hoja
+		t.localPosition = Vector3.zero;
+	}
+
+}
---
+++ Assets/Scripts/ObtenerIdioma.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ObtenerIdioma : MonoBehaviour {
+
+
+	public int indiceTexto;
+	Text texto;
+	SeleccionarIdioma idioma;
+
+	void Start()
+	{
+		idioma = (SeleccionarIdioma) FindObjectOfType(typeof(SeleccionarIdioma));
+		texto = GetComponent<Text>();
+	}
+
+	void Update()
+	{
+		//if(SeleccionarIdioma.instance != null)
+		//texto.text = SeleccionarIdioma.instance.idiomaFinal[indiceTexto];
+		texto.text = idioma.idiomaFinal[indiceTexto];
+	}
+
+}
