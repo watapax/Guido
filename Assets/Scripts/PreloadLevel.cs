6f856edf292d256bf514e47ca792a801a87eee66
Juan Correa <wuazopax@gmail.com>
2015-11-11 12:29:55 -0300

    Menu Principal

--- Assets/Scripts/PreloadLevel.cs
+++ Assets/Scripts/PreloadLevel.cs
@@ -3,23 +3,28 @@ using System.Collections;
 
 public class PreloadLevel : MonoBehaviour {
 
-	public Animator menuAnim;
+	//public Animator menuAnim;
 
 	AsyncOperation async;
 	bool puedeCargar;
 
 	void Start()
 	{
-		StartCoroutine(Preload());
+		//StartCoroutine(Preload());
 	}
 
-	public void CargarEscena(string trigger)
+	public void CargarEscena()
 	{
-		menuAnim.SetTrigger(trigger);
+		//menuAnim.SetTrigger(trigger);
 		puedeCargar = true;
 		async.allowSceneActivation = true;
 	}
 
+
+	public void LoadEscena(int index)
+	{
+		Application.LoadLevel(index);
+	}
 	IEnumerator Preload()
 	{
 		async = Application.LoadLevelAsync(1);
@@ -28,18 +33,18 @@ public class PreloadLevel : MonoBehaviour {
 		yield return async;
 	}
 
-	void Update()
-	{
-		if(Input.GetKeyDown(KeyCode.Escape))
-			Application.Quit();
-
-		if(puedeCargar)
-		{
-			if(async != null && async.isDone)
-			{
-				print ("peo");
-			}
-		}
-	}
+//	void Update()
+//	{
+//		if(Input.GetKeyDown(KeyCode.Escape))
+//			Application.Quit();
+//
+//		if(puedeCargar)
+//		{
+//			if(async != null && async.isDone)
+//			{
+//				print ("peo");
+//			}
+//		}
+//	}
 
 }
